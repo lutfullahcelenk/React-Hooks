73a9bdcd39b36ca1b87874f1e53fff78e75902f9
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-01 21:28:22 +0300

    Header component is used and p and a button tag is wirtten

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import './App.css';
 import Header from "./components/Header";
 import List from "./components/Header";
@@ -6,9 +6,16 @@ import fs from "./assets/fs.png";
 import aws from "./assets/aws.png";
 
 function App() {
+
+  const[counter,setCounter] = useState(0)
+
   return (
     <div className="App">
-  
+      
+      <Header />
+      <p>Counter: {counter}</p>
+      <button onClick={() => setCounter(counter+1)}>Increase</button>
+
     </div>
   );
 }
