a03d867793c6c8ac011ea78fd671f641ff6ed498
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-01 21:08:33 +0300

    Header component is created and img tag is written

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import cw from "../assets/cw_logo.png"
+
+const Header = () => {
+    console.log("Rendering: Header Comp!")
+    return (
+        <div className="header">
+
+            <img 
+                src={img? img : cw}
+                alt="CW Logo"
+                style={{margin:"1rem", maxHeight:"200px"}}
+            />
+            
+        </div>
+    )
+}
+
+export default Header
