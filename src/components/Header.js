9ca0855438d517589d499db1c2bf857fd45fa462
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-01 21:35:44 +0300

    3 ways of using react memo is wirtten and two of them inside comment lines

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {memo} from "react";
 import cw from "../assets/cw_logo.png";
 
 const Header = React.memo(({img}) => {
@@ -16,4 +16,57 @@ const Header = React.memo(({img}) => {
   );
 });
 
-export default Header;
+export default memo(Header);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**********REACT MEMO ILE KULLANIM***************
+
+// const Header = React.memo(({img}) => {
+
+//     console.log("Rendering: Header Comp!");
+  
+//     return (
+//       <div className="header">
+//         <img
+//           src={img ? img : cw}
+//           alt="CW_logo"
+//           style={{ margin: "1rem", maxHeight: "200px" }}
+//         />
+//       </div>
+//     );
+//   });
+
+
+
+
+//************Yine memo ile en sona ekleme */
+// const Header = React.memo(({img}) => {
+
+//     console.log("Rendering: Header Comp!");
+  
+//     return (
+//       <div className="header">
+//         <img
+//           src={img ? img : cw}
+//           alt="CW_logo"
+//           style={{ margin: "1rem", maxHeight: "200px" }}
+//         />
+//       </div>
+//     );
+//   });
+  
+//   export default React.memo(Header);
